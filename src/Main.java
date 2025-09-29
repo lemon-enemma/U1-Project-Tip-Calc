b642e6583021df23b019f0db4e340b965bd3a72f
lemon-enemma <emmal219@nycstudents.net>
2025-09-28 21:02:54 -0400

    final without proper formatting

--- src/Main.java
+++ src/Main.java
@@ -10,23 +10,53 @@ public class Main {
         scan.nextLine();
         System.out.print("Enter the percentage of the bill you want to tip: ");
         int tipPercentint = scan.nextInt();
-        double tipPercentdoub = (double) tipPercentint/100;
         System.out.print("Enter the number of people in your party: ");
         int partyPeople = scan.nextInt();
         //input part ends. calc total tip below
-        double tipUnrounded = (tipPercentdoub) * totalBill;
-        double tipIndollars = tipUnrounded % 100;
-        double tipIncents = (tipUnrounded - tipUnrounded % 100) * 100;
-        int tipIncentsRound = (int) (tipIncents + 0.5);
-        double tipCents = tipIncentsRound/100;
-        System.out.println("The total tip is $" + tipIndollars + "." + tipCents);
+        double tipUnrounded = (tipPercentint) * totalBill;
+        int tipIncentsInt = (int) ((tipUnrounded % 100) + 0.5);
+        int tipIndollarsInt = (int) (tipUnrounded - tipUnrounded % 100)/100;
+        String tipIncentsString;
+            if (tipIncentsInt < 10) {
+                tipIncentsString = "0" + tipIncentsInt;
+            System.out.println("The total tip is $" + tipIndollarsInt + "." + tipIncentsString + ".");}
+            else {
+            System.out.println("The total tip is $" + tipIndollarsInt + "." + tipIncentsInt + ".");}
         //calc total bill including tip
-        double totalPlustip = totalBill + tipIndollars;
-        System.out.println("The total bill plus tip is $" + totalPlustip);
+        double totalPlustipUnrounded = (totalBill + tipUnrounded/100) * 100;
+        int totalPlustipCentsint = (int) ((totalPlustipUnrounded % 100) + 0.5);
+        int totalPlustipDollarsint = (int) (totalPlustipUnrounded - totalPlustipUnrounded % 100)/100;
+        String totalPlustipCentsstring;
+            if (totalPlustipCentsint < 10) {
+                totalPlustipCentsstring = "0" + totalPlustipCentsint;
+                System.out.println("The total bill, plus the tip, is $" + totalPlustipDollarsint + "." + totalPlustipCentsstring + ".");
+            }
+            else {
+                System.out.println("The total bill, plus the tip, is $" + totalPlustipDollarsint + "." + totalPlustipCentsint + ".");
+            }
         //calc tip per person
-        tipIndollars = 42.70;
-        double individTipunrounded = tipIndollars / partyPeople * 100;
-        double tipPerperson = ((double) ((int) individTipunrounded+5))/100;
-        System.out.println(tipPerperson);
+        double tipPerpersonUnrounded = (tipUnrounded/100/partyPeople) * 100;
+        int tipPerpersonCentsint = (int) ((tipPerpersonUnrounded % 100) + 0.5);
+        int tipPerpersonDollarsint = (int) (tipPerpersonUnrounded - tipPerpersonUnrounded % 100)/100;
+        String tipPerpersonCentsstring;
+            if (tipPerpersonCentsint < 10) {
+                tipPerpersonCentsstring = "0" + tipPerpersonCentsint;
+                System.out.println("The tip per person is $" + tipPerpersonDollarsint + "." + tipPerpersonCentsstring + ".");
+            }
+            else {
+                System.out.println("The tip per person is $" + tipPerpersonDollarsint + "." + tipPerpersonCentsint + ".");
+            }
+        //calc total per person
+        double totalPerpersonUnrounded = (tipPerpersonUnrounded/100 + totalBill/partyPeople) * 100;
+        int totalPerpersonCentsint = (int) ((totalPerpersonUnrounded % 100) + 0.5);
+        int totalPerpersonDollarsint = (int) (totalPerpersonUnrounded - totalPerpersonUnrounded % 100)/100;
+        String totalPerpersonCentsstring;
+        if (totalPerpersonCentsint < 10) {
+            totalPerpersonCentsstring = "0" + totalPerpersonCentsint;
+            System.out.println("The total bill plus tip per person is $" + totalPerpersonDollarsint + "." + totalPerpersonCentsstring + ".");
+        }
+        else {
+            System.out.println("The total bill plus tip per person is $" + totalPerpersonDollarsint + "." + totalPerpersonCentsint + ".");
+        }
     }
 }
